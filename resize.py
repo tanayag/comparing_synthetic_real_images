7559460c61cf2d6247226ca10bdc27c1a19841f6
tanayag <tanay_agrawal@hotmail.com>
2019-04-26 14:08:05 +0530

    adding feature map visualization and code description

--- resize.py
+++ resize.py
@@ -1,4 +1,4 @@
-folder = './cvidea/validation_flir/PreviewData' # test/train
+folder = './cvidea/classification_dataset/synthetic_images'
 
 from PIL import Image
 import os
@@ -7,7 +7,7 @@ c=0
 for i in os.listdir(folder):
     try:
         img = Image.open(folder+"/"+i)
-        img = img.resize((299, 299), Image.ANTIALIAS)
+        img = img.resize((224, 224), Image.ANTIALIAS)
         img.save(folder+"/"+i)
     except:
         os.unlink(folder+"/"+i)
